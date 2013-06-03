21c92e85c8e10a739f51ea8f087521fd0ae2f66b
Frank Schmidt <frank.schmidt@mni.thm.de>
2013-06-03 15:35:42 +0200

    adapt for Kernel 3.8

--- osm/linux/install.sh
+++ osm/linux/install.sh
@@ -22,6 +22,10 @@ case ${KERNEL_VER} in
 	OBJ=ko
 	MODVER=`modinfo -F vermagic ${PWD}/${TARGETNAME}.${OBJ} | cut -d' ' -f1`
 	;;
+	3.8 )
+	OBJ=ko
+	MODVER=`modinfo -F vermagic ${PWD}/${TARGETNAME}.${OBJ} | cut -d' ' -f1`
+	;;
 esac
 
 if test "${MODVER}" = "" ; then
